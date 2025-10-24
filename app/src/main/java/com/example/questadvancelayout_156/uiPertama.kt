6e33ea2cb6d722bf802f70fd7a6cc30552425476
dannyfadli <danyfdly95@gmail.com>
2025-10-24 09:16:05 +0700

    feat: Create 'ActivitasPertama' screen structure}

---
+++ app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
@@ -0,0 +1,16 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import java.lang.reflect.Modifier
+
+
+@composable
+fun ActivitasPertama(modifier: Modifier){
+    Column (
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxWidth()
+
+    )
+}
