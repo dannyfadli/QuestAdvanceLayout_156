8b593332ca470cddeb030e6d3a716ed433ee742b
dannyfadli <danyfdly95@gmail.com>
2025-10-24 09:37:40 +0700

    memnabahkan Card dan juga mengatur layot nya  dari warna ukuran dan jarak

--- app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
@@ -1,12 +1,16 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -32,5 +36,16 @@ fun ActivitasPertama(modifier: Modifier){
             fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(height = 25.dp))
+        Card (
+            modifier = Modifier
+                .fillMaxSize(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+
+        ){
+
+        }
     }
 }
