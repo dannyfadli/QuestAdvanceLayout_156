11bc93be09fbbd3460b2e13b36badce1ec44e448
dannyfadli <danyfdly95@gmail.com>
2025-10-24 09:46:56 +0700

    memnabahkan gamabar kedalam car kemudian mengatur ukuran jarak nya

--- app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
@@ -1,9 +1,12 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -11,7 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -45,7 +50,18 @@ fun ActivitasPertama(modifier: Modifier){
             )
 
         ){
+            Row()
+            {
+                val gambar = painterResource(id= R.drawable.Veliona)
+                Image(
+                    Painter = gambar,
+                    contenDescrption = null,
+                    modifier = Modifier
+                        .size(size = 75.dp)
+                        .padding(all = 100.dp)
+                )
 
+            }
         }
     }
 }
