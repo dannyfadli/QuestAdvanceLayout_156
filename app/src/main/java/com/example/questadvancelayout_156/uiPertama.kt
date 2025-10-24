c44cbc1cd9e09653616fc0477d7664155e27d5d4
dannyfadli <danyfdly95@gmail.com>
2025-10-24 09:32:04 +0700

    memnabahkan spacer untuk memberikan jarak

--- app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
@@ -1,10 +1,13 @@
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -28,6 +31,6 @@ fun ActivitasPertama(modifier: Modifier){
             text = stringResource( id = R.string.univ),
             fontSize = 22.sp
         )
-
+        Spacer(modifier = Modifier.height(height = 25.dp))
     }
 }
