7ff9bfb6f93d1d299558bfe69b7e6c021fd23890
dannyfadli <danyfdly95@gmail.com>
2025-10-24 09:28:47 +0700

    Memamnngil variabek string resoutresce prodi kemduian mengatur layoutnya

--- app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_156/uiPertama.kt
@@ -1,12 +1,17 @@
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import java.lang.reflect.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questadvancelayout_156.R
 
-
-@composable
+@Composable
 fun ActivitasPertama(modifier: Modifier){
     Column (
         modifier = Modifier
@@ -14,6 +19,12 @@ fun ActivitasPertama(modifier: Modifier){
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        Text(
+            text = stringResource( id = R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+
+        )
 
     }
 }
